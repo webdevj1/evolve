0a89441fe1a2676e26383fc258e128802fd702de
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-03-17 16:50:11 -0400

    show pics

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+let src = 'http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/';
 
 class App extends Component {
+  state = {champs: {}};
+  componentDidMount(){
+    axios.get('http://localhost:8000', {method: 'cors'})
+    .then(res=>{
+      console.log(res)
+      this.setState({champs: res.data.data})
+  })
+    .catch(err=>console.log(err))
+
+  }
   render() {
+    console.log(Object.keys(this.state.champs))
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {Object.keys(this.state.champs).map(champ=>(
+          <div>
+             <br/>
+            <p>{champ}</p>
+            <img style={{borderRadius: '50%'}} src={src+champ+'.png'} />
+          </div>
+          
+        ))}
       </div>
     );
   }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const key = '?api_key=RGAPI-35ec265a-1dba-40db-801d-c8d5a44a45b7';
+let axios = require('axios');
+var config = {
+  headers: {'Access-Control-Allow-Origin': '*'}
+};
+const allChamps = require('../API/allChamps.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json(allChamps)
 });
 
 module.exports = router;
