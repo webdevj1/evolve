a8ccaf253e40099eed794aee36573641f38c4133
webdevj1 <zgsales@aol.com>
2018-03-18 17:47:51 -0400

    added routes for data

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,37 @@ var config = {
   headers: {'Access-Control-Allow-Origin': '*'}
 };
 const allChamps = require('../API/allChamps.js');
+const champID = require('../API/champID.js');
+const roles = require('../API/roles.js');
+const items = require('../API/items.js');
+const maps = require('../API/maps.js');
+const user_profiles = require('../API/profileIcons.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json(allChamps)
+});
+
+router.get('/champID', function(req, res, next) {
+  res.json(champID)
+});
+
+router.get('/roles', function(req, res, next) {
+  res.json(roles)
+});
+
+router.get('/items', function(req, res, next) {
+  res.json(items)
+});
+
+router.get('/maps', function(req, res, next) {
+  res.json(maps)
+});
+
+
+router.get('/profiles', function(req, res, next) {
+  res.json(user_profiles)
 });
 
 
