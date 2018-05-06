a88b604d0428a06dce65bc9620a375c0822f2286
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-05-06 11:07:30 -0400

    heroku version

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 let express = require('express');
 let router = express.Router();
-const key = '?api_key=RGAPI-35ec265a-1dba-40db-801d-c8d5a44a45b7';
 let axios = require('axios');
-var config = {
-  headers: {'Access-Control-Allow-Origin': '*'}
-};
 const allChamps = require('../API/allChamps.js');
 const champID = require('../API/champID.js');
 const roles = require('../API/roles.js');
@@ -14,34 +10,33 @@ const user_profiles = require('../API/profileIcons.js');
 const itemBuild = require('../API/itemBuild.js');
 
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json(allChamps)
+  res.json(allChamps);
 });
 
 router.get('/champID', function(req, res, next) {
-  res.json(champID)
+  res.json(champID);
 });
 
 router.get('/roles', function(req, res, next) {
-  res.json(roles)
+  res.json(roles);
 });
 
 router.get('/items', function(req, res, next) {
-  res.json(items)
+  res.json(items);
 });
 
 router.get('/itemsbuild', function(req, res, next) {
-  res.json(itemBuild)
+  res.json(itemBuild);
 });
 
 router.get('/maps', function(req, res, next) {
-  res.json(maps)
+  res.json(maps);
 });
 
 
 router.get('/profiles', function(req, res, next) {
-  res.json(user_profiles)
+  res.json(user_profiles);
 });
 
 
