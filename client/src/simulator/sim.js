7293daa7ca3b8d8bf5b11a17eb0f6186ebc563ad
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-08-17 16:06:12 -0400

    more comments

--- client/src/simulator/sim.js
+++ client/src/simulator/sim.js
@@ -17,11 +17,11 @@ class Sim extends Component{
             champsData: {},
             champs:[],
             items: [],
-            pick: 'https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png',
+            pickImage: `url("http://apollo-na-uploads.s3.amazonaws.com/1427669031664/SRBackground.png")`,
             pickName: '',
             pickItems: [],
             counters: [],
-            counter: 'https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png',
+            counterImage: `url("http://apollo-na-uploads.s3.amazonaws.com/1427669031664/SRBackground.png")`,
             counterName: '',
             counterItems: [],
             title: '',
@@ -34,7 +34,7 @@ class Sim extends Component{
         this.renderNotes = this.renderNotes.bind(this);
     }
 
-    //Load Champions, Items, & Notes after the component has been rendered
+    /* Load Champions, Items, & Notes after the component has been rendered */
     componentDidMount() {
         axios.get('http://localhost:8000')
         .then(res => {
@@ -45,70 +45,86 @@ class Sim extends Component{
         })
         .catch(err => console.log(err));
 
-        //Function to check the firebase database for the rendered notes.
+        /* Function to check the firebase database for the rendered notes. */
         database.on('value', snapshot =>{
             this.setState({notes: snapshot.val()});
             //Store all, if any, notes to state.
         });
         
-        //Getting all of the items to show along with the champions.
+        /* Getting all of the items to show along with the champions. */
         axios.get('http://localhost:8000/items')
         .then(res =>{
             this.setState({items: res.data.data});
         });
     }
 
-    //Handles the current input for new user note.
-    handleChange(e){
-        this.setState({[e.target.name]: e.target.value})
+    /* Handles the input field for the search bar. */
+    handleInput = e =>{
+        let name = e.target.value
+        
+        axios.get('http://localhost:8000/roles') //Getting list of champion names
+        .then(res => {
+            let champions = res.data.all
+            let newChamps = champions.filter(champ=>{
+                if(champ.toLowerCase().startsWith(name.toLowerCase())){
+                    return champ;
+                }
+            });
+            if(newChamps.length !== 0){
+                this.setState({champs: newChamps})
+            }
+        })
     }
 
-    //Handles the submission of a new user note.
+    /* Handles the current input for adding a note. */
+    handleChange = (e) => { this.setState({ [e.target.name]: e.target.value })};
+
+    /* Handles the submission of adding a new note to the database */
     handleSubmit(e){
         e.preventDefault();
-
-        console.log('submit clicked');
-        //Storing the note in state.
-        const note ={
-            title:this.state.title,
-            body:this.state.body
-        };
-        //Adding the new note to the firebase database.
-        database.push(note);
         
-        //Clearing the current input field
+        const note = {
+            title: this.state.title,
+            body: this.state.body
+        };
+
+        /* database.push() is the function used to add to the firebase database. */
+        database.push(note);
+
+        /* Clears the current input fields after submission. */
         this.setState({
             title:'',
-            body:''
+            body:''    
         });
-    };
+    }
+
 
     handleClick = e =>{
         const {champsData} = this.state;
-        let alt = e.target.alt; //champ's name
+        /* 
+        Champ's name was stored in the alt of the
+        element. Storing it now in the variable "alt".
+        */
+       let champName = e.target.alt; 
         
         this.setState({
-            pick: e.target.src, //changing the empty square with the champ user clicked
-            pickName: alt, //renders the clicked champ's name under the picture
-            counters: champsData[alt].counters,
-            counter: `http://ddragon.leagueoflegends.com/cdn/8.6.1/img/champion/${champsData[alt].counters[0].champion}.png`, //gives the image of the counter champ on the right
-            counterName: champsData[alt].counters[0].champion, //renders the counter champ's name under the picture on the right
-        
+            pickImage: `url('http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champName}_0.jpg')`, //Changing the background images of the empty squares with the image of the champion selected.
+            pickName: champName, //renders the clicked champ's name above their picture
+            counters: champsData[champName].counters, //Targeting the selected champ's counter opponents and storing them in state.
+            counterImage: `url('http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champsData[champName].counters[0].champion}_0.jpg')`, //Grabbing the image of the top counter. Hence the reason for the zero index.
+            counterName: champsData[champName].counters[0].champion, //Grabbing the name of the top counter and storing it in state.
         });
 
         axios.get('http://localhost:8000/itemsbuild')
         .then(res=>{
-            res.data.map(build=>{
-                let items = build.hashes.finalitemshashfixed.highestWinrate.hash.replace(/-/g, ' ').split(' '); // gets all the items to complete build
-                if(Number(champsData[alt].key) === build.championId){ // looking for the clicked champ's items
-                    this.setState({
-                        pickItems: items //storing it in the state
-                    });
+            console.log(res.data)
+            res.data.forEach(build=>{
+                let items = build.hashes.finalitemshashfixed.highestWinrate.hash.replace(/-/g, ' ').split(' '); //Coverting the string containing all the item numbers into an array. Also getting rid of all dashes and spaces.
+                if(Number(champsData[champName].key) === build.championId){ //Checking to see if current item build champion id matches the selected champion.
+                    this.setState({pickItems: items}); //Storing current champion's item build in state.
                 };
-                if(Number(champsData[champsData[alt].counters[0].champion].key) === build.championId){ //Looking for the counter champ's items
-                    this.setState({
-                        counterItems: items
-                    });
+                if(Number(champsData[champsData[champName].counters[0].champion].key) === build.championId){ //Looking for the counter champ's items
+                    this.setState({counterItems: items});
                 };
             });
         })
@@ -126,24 +142,7 @@ class Sim extends Component{
         })
     };
 
-    handleInput = e =>{
-        let name = e.target.value
-        const {champs} = this.state;
-        axios
-        .get('http://localhost:8000/roles') //getting list of champs according to lanes
-        .then(res => {
-            let champions = res.data.all
-            let newChamps = champions.filter(champ=>{
-                if(champ.toLowerCase().startsWith(name.toLowerCase())){
-                    return champ;
-                }
-            });
-            if(newChamps.length !== 0){
-                this.setState({champs: newChamps})
-            }
-        })
-        
-      }
+    
 
       //render usernotes from database
       renderNotes(){
@@ -167,46 +166,58 @@ class Sim extends Component{
         popup.classList.toggle('show');
     }
 
+    // handleFlip = e =>{
+    //     let flip = e.target.classList;
+    //     flip.toggle('flip-vertical-right');
+    //     e.target.style.backgroundImage = `url("http://apollo-na-uploads.s3.amazonaws.com/1427669031664/SRBackground.png")`;
+    //     let all = document.querySelectorAll('.items_container');
+    //     all.forEach(el=>{
+    //         el.style.display = 'inline';
+    //     })
+    // }
+
     render(){
-        const {champsData, champs, items, pick, pickName, counter, counters, counterName, pickItems, counterItems, userInputChamp} = this.state;
+        const {champsData, champs, pickImage, pickName, counterImage, counters, counterName, pickItems, counterItems, userInputChamp} = this.state;
         let hide = !pickName?'none':'';
         
-        let pickImage = !pickName? `url("http://apollo-na-uploads.s3.amazonaws.com/1427669031664/SRBackground.png")` : `url('http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${pickName}_0.jpg')`;
-        let counterImage = !counterName? `url("http://apollo-na-uploads.s3.amazonaws.com/1427669031664/SRBackground.png")` : `url('http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${counterName}_0.jpg')`;
         return(
             <div>
 
-                <input onChange={this.handleInput} className="summonername" placeholder="Search for a champion"/>
+                <input onChange={this.handleInput} className="summonername" placeholder="Search for a champion"/> {/* Input field at the top of the page that allows you to search for a specific champion. */}
                 <br/>
                 <br/>
                 <div className="allroles">
                     <img onClick={this.handleRoles} name="all" className='roles' src={allChamps} alt="all" />
                     <img onClick={this.handleRoles} name="top" className='roles' src={top} alt="top"/>
                     <img onClick={this.handleRoles} name="mid" className='roles' src={mid} alt="mid"/>       {/*  All of the lanes available  */}
-                    <img onClick={this.handleRoles} name="support" className='roles' src={support} alt="support"/>
+                    <img onClick={this.handleRoles} name="support" className='roles' src={support} alt="support"/>   {/*  All lane images are downloaded and imported at the top.  */}
                     <img onClick={this.handleRoles} name="bot" className='roles' src={bottom} alt="bot"/>
                     <img onClick={this.handleRoles} name="jungle" className='roles' src={jungle} alt="jungle"/> {" "}
                 </div>
           
       
                 <div id="simulator">
-                    <div style={{backgroundImage: pickImage}} id="choice1" className={["choices"].join(' ')}>
-                        <div className="goodWith">
-                        <div style={{border: pickName? 'none': ''}} className="enemypick">{!pickName ? "Click to reveal a champion counter":"" }</div>
-                            <div id="pick" className="info">    
+                    <div style={{backgroundImage: pickImage}} className={"choices"}>
+                        <div className="pick">
+                            <div style={{border: pickName? 'none': ''}} className="champion">{!pickName ? "Click to reveal a champion and its counter":"" }</div>{/* Displayed before any champion is clicked on. Hidden after. */}
+                            <div id="choosen" className="info">    
                                 {pickName? <p className="pickname">{champsData[pickName].name}</p> : ''}
+                                {/* Shows to selected champion's name. */}
                             </div>
                         </div>
-                        {pickItems.length > 0 ? <p className="itemname">Suggested Item Build </p> : ''}
+                        {pickItems.length ? <p className="item-title">Suggested Item Build </p> : ''}
                         {pickItems.map((item, key)=>{
-
-                            if(!isNaN(Number(item))){ //items list includes the word item... Just making sure to ignore it and just focus on the actual item numbers
+                            if(!isNaN(Number(item))){
+                                /*
+                                items list includes the word "item"... 
+                                Just making sure to ignore it and just focus on the actual item numbers
+                                */
                                 return(
                                     <div className='items_container' >
                                         <div className='popup'>
                                             <span className="popuptext" id={item}>
                                             </span>
-                                            <img onMouseOver={this.handleItems} onMouseOut={this.handleItems} className={['items'].join(' ')} src={`http://ddragon.leagueoflegends.com/cdn/8.6.1/img/item/${item}.png`} alt={item} />
+                                            <img onMouseOver={this.handleItems} onMouseOut={this.handleItems} className={'items'} src={`http://ddragon.leagueoflegends.com/cdn/8.6.1/img/item/${item}.png`} alt={item} />
                                         </div>
                                     </div>
                                 )
@@ -218,12 +229,12 @@ class Sim extends Component{
                             <img onClick={this.handleClick} className="choose grow" src={`http://ddragon.leagueoflegends.com/cdn/8.6.1/img/champion/${champ}.png`} alt={champ} key={key} />
                         ))}       
                     </div>
-                    <div style={{backgroundImage: counterImage}} id="choice2" className="choices">
+                    <div style={{backgroundImage: counterImage}} className="choices">
                         <div className="counter">
-                        <div style={{border: pickName? 'none': ''}} className="enemypick">{!pickName? !pickName ? "Counter Pick":"" : ''}</div>
+                        <div style={{border: pickName? 'none': ''}} className="champion">{!pickName? !pickName ? "Counter Pick":"" : ''}</div>
                            {counterName? <p className="counterpick">{champsData[counterName].name}</p> : "" }
                         </div>
-                        {counterItems.length > 0 ? <p className="itemname">Suggested Item Build</p> : ''}
+                        {counterItems.length > 0 ? <p className="item-title">Suggested Item Build</p> : ''}
                         {counterItems.map(item=>{
                             if(!isNaN(Number(item))){ //items list includes the word item... Just making sure to ignore it and just focus on the actual item numbers
                                 return(
@@ -231,12 +242,12 @@ class Sim extends Component{
                                         <div className='popup'>
                                             <span className="popuptext" id={item}>
                                             </span>
-                                            <img onMouseOver={this.handleItems} onMouseOut={this.handleItems} className={['items'].join(' ')} src={`http://ddragon.leagueoflegends.com/cdn/8.6.1/img/item/${item}.png`} alt={item} />
+                                            <img onMouseOver={this.handleItems} onMouseOut={this.handleItems} className={'items'} src={`http://ddragon.leagueoflegends.com/cdn/8.6.1/img/item/${item}.png`} alt={item} />
                                         </div>
                                     </div>
                                 )
                             }
-                        })} */}
+                        })}
                     </div>
                 </div>
                 <p className="morecounters" style={{display: hide}}>Additional Counters</p>
@@ -245,8 +256,6 @@ class Sim extends Component{
                             <div className="more_counters">
                                 <p>{champsData[champ.champion].name}</p>
                                 <div style={{backgroundImage: `url('http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.champion}_0.jpg')`}} className="more_choices grow">
-                                    
-                                    
                                 </div>
                             </div>
                         ))}        
@@ -266,7 +275,6 @@ class Sim extends Component{
 
                         </a>
                 </div>
-
                 <br/>
 
 <div className="container-fluid">
@@ -348,7 +356,6 @@ class Sim extends Component{
     </div>
 
 </div>
-
             </div>
     );
   };
