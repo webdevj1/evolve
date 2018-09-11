95c21310d4ecb3906bfe3e095a75f34b92f2f16f
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-09-11 11:08:19 -0400

    bugs

--- app.js
+++ app.js
@@ -21,11 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 app.use('/', index);
 app.use('/users', users);
