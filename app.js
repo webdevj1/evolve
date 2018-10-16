70db0b4b78afe9135f75af1fb74017c2fd02a5f7
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-10-16 12:26:38 -0400

    heroku

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build', 'index.html')));
+app.use(express.static(path.join(__dirname, './client/build', 'index.html')));
 app.use(cors())
 
 app.get('*', (request, response) => {
